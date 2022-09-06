49aed30bb8af2126c03e61f7f8bc0102204be428
Malton <ymatsubayashi09@outlook.jp>
2022-09-06 23:28:23 +0900

    update README / add skip mode

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Random Box 2
 それっぽいアニメーションとともに　あ～わ　までのひらがながランダムで出ます
 
-## 選択肢の指定
-urlのパラメーターで選択肢を指定できます。  
-例：https://randomdice2.netlify.app/?e=あ,い,う  
+## パラメーター
 
-また、rangeの指定も可能です。  
+自由選択肢
+例：https://randomdice2.netlify.app/?e=りんご,みかん,ぶどう  
+
+数値の範囲指定
 例：https://randomdice2.netlify.app/?range=1,20  
+
+効果音の有無
+例：https://randomdice2.netlify.app/?range=1,20&sound=true
+
+アニメーションをスキップ
+例：https://randomdice2.netlify.app/?range=1,20&skip=true
--- main.js
+++ main.js
@@ -33,26 +33,29 @@ else if(url_params['e']){
 console.log(election_box)
 
 sound1=new Audio('sound/sound1.mp3')
-//sound2=new Audio(sound/sound2.mp3')
+sound2=new Audio('sound/sound2.mp3')
 
-sound2=new Audio('sound/o.m4a')
-
-sound1.volume = 0.2;
 
 document.getElementById('BOX').addEventListener('click',()=>{
-    PlayAnimation()
-    setTimeout(()=>{
-        PouseAnimation()
-    },10*1000)
-    if(url_params['sound']=="true"){
-        sound1.play()
+    if(url_params['skip']=="true"){
+        random_chara=election_box[Math.floor(Math.random()*election_box.length)]
+        chara.innerText=random_chara
+        chara.style.fontSize=240/random_chara.length+'px'
+    }else{
+        PlayAnimation()
         setTimeout(()=>{
-            sound1.pause()
-            sound1.currentTime=0
-            sound2.play()
-        },4.1*1000)
+            PouseAnimation()
+        },10*1000)
+        if(url_params['sound']=="true"){
+            sound1.play()
+            setTimeout(()=>{
+                sound1.pause()
+                sound1.currentTime=0
+                sound2.play()
+            },4.1*1000)
+        }
+        random_chara=election_box[Math.floor(Math.random()*election_box.length)]
+        chara.innerText=random_chara
+        chara.style.fontSize=240/random_chara.length+'px'
     }
-    random_chara=election_box[Math.floor(Math.random()*election_box.length)]
-    chara.innerText=random_chara
-    chara.style.fontSize=240/random_chara.length+'px'
 })
